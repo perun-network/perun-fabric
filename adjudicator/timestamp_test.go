dfcf0c426646917bf88b5ceb671874233cd72665
Sebastian Stammler <seb@perun.network>
2022-03-10 19:53:03 +0100

    adj: Improve StdTimestamp test
    The CI failed once apparently because both testing timestamps t0 and t1
    were the same.

--- adjudicator/timestamp_test.go
+++ adjudicator/timestamp_test.go
@@ -4,14 +4,27 @@ package adjudicator_test
 
 import (
 	"testing"
-	"time"
 
 	adj "github.com/perun-network/perun-fabric/adjudicator"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestStdTime(t *testing.T) {
-	t0, t1 := adj.StdNow(), adj.StdNow()
+func TestStdTimestamp(t *testing.T) {
+	t.Run("Add", func(t *testing.T) {
+		t0 := adj.StdNow()
+		t1 := t0.Add(1)
+		assert.True(t, t1.After(t0))
+	})
+
+	t0 := adj.StdNow()
+
+	t.Run("Clone", func(t *testing.T) {
+		t0c := t0.Clone()
+		t0c = t0c.Add(1)
+		assert.False(t, t0c.Equal(t0))
+	})
+
+	t1 := adj.StdNow()
 
 	t.Run("Equal", func(t *testing.T) {
 		assert.True(t, t0.Equal(t0))
@@ -29,17 +42,5 @@ func TestStdTime(t *testing.T) {
 		assert.True(t, t0.Before(t1))
 		assert.False(t, t0.Before(t0))
 		assert.False(t, t1.Before(t0))
-	})
-
-	t.Run("Add", func(t *testing.T) {
-		t0 := adj.StdNow()
-		t1 := t0.Add(1)
-		assert.True(t, t1.After(t0))
-	})
-
-	t.Run("Clone", func(t *testing.T) {
-		t0c := t0.Clone().(adj.StdTimestamp)
-		t0c = (adj.StdTimestamp)(t0c.Time().Add(time.Hour))
-		assert.False(t, t0c.Equal(t0))
 	})
 }
