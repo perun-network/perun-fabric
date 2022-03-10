c9e0f61c155f1c52010dca61cb212b2a9a1c320e
Sebastian Stammler <seb@perun.network>
2022-03-10 15:38:32 +0100

    adj: Add methods State.Total,CoreState

--- adjudicator/types.go
+++ adjudicator/types.go
@@ -1,7 +1,8 @@
 package adjudicator
 
 import (
-	"perun.network/go-perun/backend/ethereum/wallet"
+	"math/big"
+
 	ethwallet "perun.network/go-perun/backend/ethereum/wallet"
 	"perun.network/go-perun/channel"
 	"perun.network/go-perun/wallet"
@@ -65,6 +66,36 @@ func AsWalletAddresses(as []Address) []wallet.Address {
 	}
 	return was
 }
+
+// CoreState returns the equivalent representation of s as channel.State.
+// The returned State is set to have no App, contains one asset that is default
+// initialized and this first assets' balances are set to the Balances of s.
+//
+// Use the State returned by CoreState to create or verify signatures with the
+// go-perun channel backend.
+//
+// It is not a deep copy, e.g., field Balances references the same balances
+// slice.
+func (s State) CoreState() *channel.State {
+	return &channel.State{
+		ID:      s.ID,
+		Version: s.Version,
+		App:     channel.NoApp(),
+		Allocation: channel.Allocation{
+			Assets:   []channel.Asset{channel.NewAsset()},
+			Balances: channel.Balances{s.Balances},
+		},
+	}
+}
+
+func (s State) Total() channel.Bal {
+	total := new(big.Int)
+	for _, bal := range s.Balances {
+		total.Add(total, bal)
+	}
+	return total
+}
+
 func (s State) Clone() State {
 	bals := channel.CloneBals(s.Balances)
 	s.Balances = bals
