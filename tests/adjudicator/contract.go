73abb177718afc17e3408d2aea26c8cb06a1ac75
Sebastian Stammler <seb@perun.network>
2022-03-17 14:34:39 +0100

    tests: Complete Adjudicator end2end test

--- tests/adjudicator/contract.go
+++ tests/adjudicator/contract.go
@@ -16,10 +16,12 @@ import (
 )
 
 const (
-	txDeposit  = "Deposit"
-	txHolding  = "Holding"
-	txRegister = "Register"
-	txWithdraw = "Withdraw"
+	txDeposit      = "Deposit"
+	txHolding      = "Holding"
+	txTotalHolding = "TotalHolding"
+	txRegister     = "Register"
+	txStateReg     = "StateReg"
+	txWithdraw     = "Withdraw"
 )
 
 type Adjudicator struct {
@@ -47,6 +49,14 @@ func (a *Adjudicator) Holding(id channel.ID, addr wallet.Address) (*big.Int, err
 	return tests.BigIntWithError(a.Contract.SubmitTransaction(txHolding, args...))
 }
 
+func (a *Adjudicator) TotalHolding(id channel.ID, addrs []wallet.Address) (*big.Int, error) {
+	args, err := pkgjson.MultiMarshal(id, addrs)
+	if err != nil {
+		return nil, err
+	}
+	return tests.BigIntWithError(a.Contract.SubmitTransaction(txTotalHolding, args...))
+}
+
 func (a *Adjudicator) Register(ch *adj.SignedChannel) error {
 	arg, err := json.Marshal(ch)
 	if err != nil {
@@ -54,6 +64,19 @@ func (a *Adjudicator) Register(ch *adj.SignedChannel) error {
 	}
 	_, err = a.Contract.SubmitTransaction(txRegister, string(arg))
 	return err
+}
+
+func (a *Adjudicator) StateReg(id channel.ID) (*adj.StateReg, error) {
+	arg, err := json.Marshal(id)
+	if err != nil {
+		return nil, err
+	}
+	regJson, err := a.Contract.SubmitTransaction(txStateReg, string(arg))
+	if err != nil {
+		return nil, err
+	}
+	var reg adj.StateReg
+	return &reg, json.Unmarshal(regJson, &reg)
 }
 
 func (a *Adjudicator) Withdraw(id channel.ID, addr wallet.Address) (*big.Int, error) {
--- tests/adjudicator/main.go
+++ tests/adjudicator/main.go
@@ -5,10 +5,11 @@ package main
 import (
 	"flag"
 	"log"
-
-	"polycry.pt/poly-go/test"
+	"math/big"
 
 	_ "perun.network/go-perun/backend/ethereum" // init ethereum backend
+	"perun.network/go-perun/channel"
+	"polycry.pt/poly-go/test"
 
 	adjtest "github.com/perun-network/perun-fabric/adjudicator/test"
 	"github.com/perun-network/perun-fabric/tests"
@@ -32,26 +33,43 @@ func main() {
 	a := NewAdjudicator(network)
 
 	rng := test.Prng(test.NameStr("FabricAdjudicator"))
-	setup := adjtest.NewSetup(rng)
+	setup := adjtest.NewSetup(rng, adjtest.WithBalances(big.NewInt(4000), big.NewInt(1000)))
 	ch, id := setup.SignedChannel(), setup.State.ID
 	log.Printf("Depositing channel: %+v", ch)
-	for i, part := range setup.Params.Parts {
+	for i, part := range setup.Parts {
 		bal := setup.State.Balances[i]
 		tests.FatalClientErr("sending Deposit tx", a.Deposit(id, part, bal))
 
 		holding, err := a.Holding(id, part)
 		tests.FatalClientErr("querying holding", err)
-		log.Printf("Querying holding: %v", holding)
+		log.Printf("Queried holding[%d]: %v", i, holding)
 		tests.RequireEqual(bal, holding, "Holding")
 	}
+	log.Print("Registering state 0")
 	tests.FatalClientErr("registering state 0", a.Register(ch))
 
-	// TODO
-	// - refute with final within timeout
-	// - withdraw for both users
+	setup.State.Version = 5
+	setup.State.IsFinal = true
+	// transfer 1000 from participant 0 to 1
+	setup.State.Balances = []channel.Bal{big.NewInt(3000), big.NewInt(2000)}
+	chfinal, regfinal := setup.SignedChannel(), setup.StateReg()
+	log.Print("Refuting with final state 5")
+	tests.FatalClientErr("registering final state 5", a.Register(chfinal))
 
-	//withdrawn, err := ah.Withdraw(id, addr)
-	//tests.FatalClientErr("withdrawing", err)
-	//log.Printf("Withdrawing: %v", withdrawn)
-	//tests.RequireEqual(holding, withdrawn, "Withdraw")
+	log.Print("Checking final state")
+	regfinal0, err := a.StateReg(id)
+	tests.FatalClientErr("querying state", err)
+	tests.RequireEqual(regfinal, regfinal0, "final StateReg")
+
+	for i, part := range setup.Parts {
+		withdrawn, err := a.Withdraw(id, part)
+		tests.FatalClientErr("withdrawing", err)
+		log.Printf("Withdrawn[%d]: %v", i, withdrawn)
+		tests.RequireEqual(setup.State.Balances[i], withdrawn, "Withdraw")
+	}
+
+	log.Print("Checking that final holding is zero")
+	totalfinal, err := a.TotalHolding(id, setup.Parts)
+	tests.FatalClientErr("querying total holding", err)
+	tests.RequireEqual(new(big.Int), totalfinal, "final zero holding")
 }
