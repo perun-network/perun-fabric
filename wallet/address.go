1b96bdd9537fc8372fcd6fa02e6e5def146c359a
Sebastian Stammler <seb@perun.network>
2022-03-18 18:03:18 +0100

    wallet: Implement Address

---
+++ wallet/address.go
@@ -0,0 +1,140 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package wallet
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"encoding/asn1"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"math/big"
+
+	"perun.network/go-perun/wallet"
+)
+
+// Address identifies a Fabric identity by its X509 certificate's public key.
+// The only supported type is *ecdsa.PublicKey because that's the only type used
+// in Fabric currently.
+type Address ecdsa.PublicKey
+
+func (a *Address) ECDSA() *ecdsa.PublicKey { return (*ecdsa.PublicKey)(a) }
+
+func (a *Address) Clone() *Address {
+	return &Address{
+		X:     new(big.Int).Set(a.X),
+		Y:     new(big.Int).Set(a.Y),
+		Curve: a.Curve, // curves are global pointers
+	}
+}
+
+type ecdsaPK struct {
+	X     *big.Int
+	Y     *big.Int
+	Curve string `asn1:"printable"`
+}
+
+func (a Address) marshalable() ecdsaPK {
+	return ecdsaPK{
+		X:     a.X,
+		Y:     a.Y,
+		Curve: a.Curve.Params().Name,
+	}
+}
+
+func (a *Address) unmarshal(data ecdsaPK) (err error) {
+	a.X = data.X
+	a.Y = data.Y
+	a.Curve, err = ecdsaCurveFromName(data.Curve)
+	return
+}
+
+func (a *Address) MarshalJSON() ([]byte, error) {
+	return json.Marshal(a.marshalable())
+}
+
+func (a *Address) UnmarshalJSON(data []byte) error {
+	var pk ecdsaPK
+	if err := json.Unmarshal(data, &pk); err != nil {
+		return err
+	}
+	return a.unmarshal(pk)
+}
+
+func (a *Address) MarshalBinary() (data []byte, err error) {
+	return asn1.Marshal(a.marshalable())
+}
+
+func (a *Address) UnmarshalBinary(data []byte) error {
+	var pk ecdsaPK
+	if rest, err := asn1.Unmarshal(data, &pk); err != nil {
+		return err
+	} else if l := len(rest); l > 0 {
+		return fmt.Errorf("unexptected rest of length %d during asn1-unmarshaling", l)
+	}
+	return a.unmarshal(pk)
+}
+
+func ecdsaCurveFromName(curve string) (elliptic.Curve, error) {
+	switch curve {
+	case "P-224":
+		return elliptic.P224(), nil
+	case "P-256":
+		return elliptic.P256(), nil
+	case "P-384":
+		return elliptic.P384(), nil
+	case "P-521":
+		return elliptic.P521(), nil
+	}
+	return nil, fmt.Errorf("unknown curve: %s", curve)
+}
+
+func (a *Address) String() string {
+	return hex.EncodeToString(elliptic.MarshalCompressed(a.Curve, a.X, a.Y))
+}
+
+// Equal returns wether the two addresses are equal. The implementation
+// must be equivalent to checking `Address.Cmp(Address) == 0`.
+func (a *Address) Equal(other wallet.Address) bool {
+	return a.ECDSA().Equal(asECDSA(other))
+}
+
+// Cmp checks the ordering of two Addresses according to following definition:
+//   -1 if (a.X <  b.X) || ((a.X == b.X) && (a.Y < b.Y))
+//    0 if (a.X == b.X) && (a.Y == b.Y)
+//   +1 if (a.X >  b.X) || ((a.X == b.X) && (a.Y > b.Y))
+// So the X coordinate takes precedence over the Y coordinate.
+// Pancis if the passed address is of the wrong type or the curves are not the
+// same.
+func (a *Address) Cmp(b wallet.Address) int {
+	other := asECDSA(b)
+	if a.ECDSA().Curve != other.Curve {
+		panic("different ECDSA curves")
+	}
+	if xCmp := a.X.Cmp(other.X); xCmp != 0 {
+		return xCmp
+	}
+	return a.Y.Cmp(other.Y)
+}
+
+func asECDSA(a wallet.Address) *ecdsa.PublicKey {
+	return ((a).(*Address)).ECDSA()
+}
+
+var defaultCurve = elliptic.P256()
+
+// NewRandomAddress creates a new Address using the randomness
+// provided by rng. The default curve (P-256) is used.
+func NewRandomAddress(rng io.Reader) *Address {
+	sk, err := ecdsa.GenerateKey(defaultCurve, rng)
+	if err != nil {
+		panic("error generating ECDSA secret key: " + err.Error())
+	}
+	return &Address{
+		Curve: sk.Curve,
+		X:     sk.X,
+		Y:     sk.Y,
+	}
+}
---
+++ wallet/address_test.go
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package wallet_test
+
+import (
+	"crypto/elliptic"
+	"math/big"
+	"testing"
+
+	wiretest "perun.network/go-perun/wire/test"
+	"polycry.pt/poly-go/test"
+
+	"github.com/perun-network/perun-fabric/wallet"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAddressJSONMarshaling(t *testing.T) {
+	rng := test.Prng(t)
+	a := wallet.NewRandomAddress(rng)
+	aj, err := a.MarshalJSON()
+	require.NoError(t, err)
+	a0 := new(wallet.Address)
+	require.NoError(t, a0.UnmarshalJSON(aj))
+	require.True(t, a.Equal(a0))
+}
+
+func TestAddressBinaryMarshaling(t *testing.T) {
+	rng := test.Prng(t)
+	wiretest.GenericMarshalerTest(t, wallet.NewRandomAddress(rng))
+}
+
+var one = big.NewInt(1)
+
+func incOne(x *big.Int) { x.Add(x, one) }
+func decOne(x *big.Int) { x.Sub(x, one) }
+
+func TestAddressCmp(t *testing.T) {
+	rng := test.Prng(t)
+	tests := []struct {
+		name   string
+		mod    func(*wallet.Address)
+		expCmp int
+	}{
+		{
+			name:   "=",
+			mod:    func(*wallet.Address) {},
+			expCmp: 0,
+		},
+		{
+			name:   "X-1",
+			mod:    func(a *wallet.Address) { decOne(a.X) },
+			expCmp: -1,
+		},
+		{
+			name:   "X+1",
+			mod:    func(a *wallet.Address) { incOne(a.X) },
+			expCmp: 1,
+		},
+		{
+			name:   "Y-1",
+			mod:    func(a *wallet.Address) { decOne(a.Y) },
+			expCmp: -1,
+		},
+		{
+			name:   "Y+1",
+			mod:    func(a *wallet.Address) { incOne(a.Y) },
+			expCmp: 1,
+		},
+		{
+			name:   "X-1,Y-1",
+			mod:    func(a *wallet.Address) { decOne(a.X); decOne(a.Y) },
+			expCmp: -1,
+		},
+		{
+			name:   "X+1,Y+1",
+			mod:    func(a *wallet.Address) { incOne(a.X); incOne(a.Y) },
+			expCmp: 1,
+		},
+		{
+			name:   "X-1,Y+1",
+			mod:    func(a *wallet.Address) { decOne(a.X); incOne(a.Y) },
+			expCmp: -1,
+		},
+		{
+			name:   "X+1,Y-1",
+			mod:    func(a *wallet.Address) { incOne(a.X); decOne(a.Y) },
+			expCmp: 1,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			a := wallet.NewRandomAddress(rng)
+			b := a.Clone()
+			tt.mod(b)
+			require.Equal(t, tt.expCmp, b.Cmp(a))
+		})
+	}
+
+	t.Run("panicOnDifferentCurve", func(t *testing.T) {
+		a := wallet.NewRandomAddress(rng)
+		b := a.Clone()
+		b.Curve = elliptic.P224() // not default P-256
+		require.Panics(t, func() {
+			a.Cmp(b)
+		})
+	})
+}
