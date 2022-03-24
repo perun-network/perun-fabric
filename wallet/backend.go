2b3137080379beee6145cddd741e0ae339cedb46
Sebastian Stammler <seb@perun.network>
2022-03-24 11:51:43 +0100

    wallet: Implement wallet.Backend and Randomizer

---
+++ wallet/backend.go
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package wallet
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+	"io"
+
+	"perun.network/go-perun/wallet"
+)
+
+type Backend struct{}
+
+// NewAddress returns a variable of type Address, which can be used
+// for unmarshalling an address from its binary representation.
+func (b Backend) NewAddress() wallet.Address {
+	return new(Address)
+}
+
+// DecodeSig reads a signature from the provided reader.
+func (b Backend) DecodeSig(r io.Reader) (wallet.Sig, error) {
+	return DecodeSig(r)
+}
+
+// VerifySignature verifies that signature sig is a valid signature by a on
+// message msg.
+// If the signature does not match the address, it returns false, nil.
+func (b Backend) VerifySignature(msg []byte, sig wallet.Sig, a wallet.Address) (bool, error) {
+	r, s, err := unmarshalSig(sig)
+	if err != nil {
+		return false, fmt.Errorf("unmarshaling sig: %w", err)
+	}
+	return ecdsa.Verify(asECDSA(a), Hash(msg), r, s), nil
+}
---
+++ wallet/randomizer.go
@@ -0,0 +1,32 @@
+package wallet
+
+import (
+	"math/rand"
+
+	"perun.network/go-perun/wallet"
+	"perun.network/go-perun/wallet/test"
+)
+
+type Randomizer struct {
+	wallet Wallet
+}
+
+func NewRandomizer() *Randomizer {
+	return &Randomizer{
+		wallet: NewWallet(),
+	}
+}
+
+// NewRandomAddress should return a new random address generated from the
+// passed rng.
+func (r *Randomizer) NewRandomAddress(rng *rand.Rand) wallet.Address {
+	return NewRandomAddress(rng)
+}
+
+// RandomWallet returns a fixed random wallet that is part of the
+// randomizer's state.
+func (r *Randomizer) RandomWallet() test.Wallet { return r.wallet }
+
+// NewWallet returns a fresh, temporary Wallet that doesn't hold any
+// accounts yet.
+func (r *Randomizer) NewWallet() test.Wallet { return NewWallet() }
