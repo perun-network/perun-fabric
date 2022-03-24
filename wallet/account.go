95b46ef89af0840e977f162a5b6c8f9bec0f2dde
Sebastian Stammler <seb@perun.network>
2022-03-24 11:47:38 +0100

    wallet: Account: Change signature marshaling to custom
    The go-perun wallet backend needs a DecodeSig function that reads from
    an io.Reader. This is unfortunately not possible with ASN1 DER encoded
    signatures. Or at least, there's no DER decoder implementation reading
    from an io.Reader.

--- wallet/account.go
+++ wallet/account.go
@@ -4,9 +4,13 @@ package wallet
 
 import (
 	"crypto/ecdsa"
+	"crypto/elliptic"
 	"crypto/rand"
 	"crypto/sha256"
+	"errors"
+	"fmt"
 	"io"
+	"math/big"
 
 	"perun.network/go-perun/wallet"
 )
@@ -40,11 +44,56 @@ func (a *Account) FabricAddress() *Address {
 
 // SignData signs the data with this account. The data is hashed before signing.
 func (a *Account) SignData(data []byte) ([]byte, error) {
-	return ecdsa.SignASN1(rand.Reader, a.ECDSA(), Hash(data))
+	r, s, err := ecdsa.Sign(rand.Reader, a.ECDSA(), Hash(data))
+	if err != nil {
+		return nil, fmt.Errorf("ecdsa.Sign: %w", err)
+	}
+	return marshalSig(a.Curve, r, s), nil
 }
 
 // Hash returns the SHA256 of msg.
 func Hash(msg []byte) []byte {
 	h := sha256.Sum256(msg)
 	return h[:]
+}
+
+func marshalSig(curve elliptic.Curve, r, s *big.Int) wallet.Sig {
+	ps := pointByteSize(curve)
+	sig := make([]byte, sigSize(ps))
+	sig[0] = ps
+	r.FillBytes(sig[1 : ps+1])
+	s.FillBytes(sig[ps+1 : 2*ps+1])
+	return sig
+}
+
+func unmarshalSig(sig []byte) (r, s *big.Int, err error) {
+	if len(sig) == 0 {
+		return nil, nil, errors.New("UnmarshalSig: empty signature")
+	}
+	ps, r, s := sig[0], new(big.Int), new(big.Int)
+	if l, ss := len(sig), sigSize(ps); l != ss {
+		return nil, nil, fmt.Errorf("UnmarshalSig: signature has wrong length %d, expected %d", l, ss)
+	}
+	r.SetBytes(sig[1 : ps+1])
+	s.SetBytes(sig[ps+1 : 2*ps+1])
+	return r, s, nil
+}
+
+func DecodeSig(r io.Reader) (wallet.Sig, error) {
+	sig := make([]byte, 1)
+	if _, err := io.ReadFull(r, sig); err != nil {
+		return nil, fmt.Errorf("reading sig size: %w", err)
+	}
+	sig = append(sig, make([]byte, sig[0]*2)...)
+	_, err := io.ReadFull(r, sig[1:])
+	return sig, err
+}
+
+func pointByteSize(curve elliptic.Curve) byte {
+	// rounding up is necessary for P521
+	return byte((curve.Params().BitSize + 7) / 8)
+}
+
+func sigSize(pointByteSize byte) int {
+	return int(pointByteSize)*2 + 1
 }
