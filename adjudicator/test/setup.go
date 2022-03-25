b1c2cb52669715b3c0946fada0febee103de9452
Sebastian Stammler <seb@perun.network>
2022-03-25 16:17:39 +0100

    adj/test: Add WithAccounts SetupOption
    It required a refactor of how SetupOptions are implemented, because
    accounts are set before the Setup struct is created.

--- adjudicator/test/setup.go
+++ adjudicator/test/setup.go
@@ -25,12 +25,43 @@ type (
 		Adj    *adj.Adjudicator
 	}
 
-	SetupOption func(*Setup)
+	SetupOption interface {
+		setupOption()
+	}
+
+	withAccsOption struct {
+		accs []wallet.Account
+	}
+
+	setupModOption interface {
+		SetupOption
+		modify(*Setup)
+	}
+
+	setupModifier func(*Setup)
 )
+
+func (setupModifier) setupOption()      {}
+func (f setupModifier) modify(s *Setup) { f(s) }
+
+func (withAccsOption) setupOption() {}
 
 func NewSetup(rng *rand.Rand, opts ...SetupOption) *Setup {
 	w := wtest.NewWallet()
-	accs := []wallet.Account{w.NewRandomAccount(rng), w.NewRandomAccount(rng)}
+
+	var accs []wallet.Account
+	// static setup options
+	for _, op := range opts {
+		switch staticOp := op.(type) {
+		case withAccsOption:
+			accs = staticOp.accs
+		}
+	}
+	// generate accs if not set by option
+	if accs == nil {
+		accs = []wallet.Account{w.NewRandomAccount(rng), w.NewRandomAccount(rng)}
+	}
+
 	parts := []wallet.Address{accs[0].Address(), accs[1].Address()}
 	params := &adj.Params{
 		ChallengeDuration: 60,
@@ -53,7 +84,10 @@ func NewSetup(rng *rand.Rand, opts ...SetupOption) *Setup {
 	}
 
 	for _, opt := range opts {
-		opt(s)
+		mod, ok := opt.(setupModOption)
+		if ok {
+			mod.modify(s)
+		}
 	}
 
 	return s
@@ -76,32 +110,36 @@ func (s *Setup) StateReg() *adj.StateReg {
 	}
 }
 
-func Funded(s *Setup) {
+var Funded = setupModifier(func(s *Setup) {
 	id := s.State.ID
 	for i, part := range s.Parts {
 		if err := s.Adj.Deposit(id, part, s.State.Balances[i]); err != nil {
 			panic(fmt.Sprintf("Setup: error funding participant[%d]: %v", i, err))
 		}
 	}
-}
+})
 
-func WithFinalState(s *Setup) {
+var WithFinalState = setupModifier(func(s *Setup) {
 	s.State.IsFinal = true
-}
+})
 
 func WithVersion(v uint64) SetupOption {
-	return func(s *Setup) {
+	return setupModifier(func(s *Setup) {
 		s.State.Version = v
-	}
+	})
 }
 
 func WithBalances(bals ...channel.Bal) SetupOption {
-	return func(s *Setup) {
+	return setupModifier(func(s *Setup) {
 		if n := len(s.Params.Parts); len(bals) != n {
 			panic(fmt.Sprintf(
 				"Setup: balances mismatches number of participants (%d != %d)",
 				len(bals), n))
 		}
 		s.State.Balances = bals
-	}
+	})
+}
+
+func WithAccounts(accs ...wallet.Account) SetupOption {
+	return withAccsOption{accs: accs}
 }
