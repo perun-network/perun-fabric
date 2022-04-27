5f70bd157d8b299143bf79c3cd14f0f996aa0847
Leandro Rometsch <leandro@perun.network>
2022-04-27 16:45:17 +0200

    funder: Improve transaction context handling
    Signed-off-by: Leandro Rometsch <leandro@perun.network>

--- channel/funder.go
+++ channel/funder.go
@@ -3,7 +3,7 @@ package channel
 import (
 	"context"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
-	assset "github.com/perun-network/perun-fabric/chaincode"
+	asset "github.com/perun-network/perun-fabric/chaincode"
 	"log"
 	"math/big"
 	pchannel "perun.network/go-perun/channel"
@@ -13,16 +13,14 @@ import (
 const defaultPollingInterval = 1 * time.Second // TODO: Consider a better place for the constant
 
 type Funder struct {
-	contract *assset.AssetHolder
-	tctx     *contractapi.TransactionContext
+	contract *asset.AssetHolder
 	polling  time.Duration
 }
 
 // NewFunder returns a new Funder.
-func NewFunder(c contractapi.Contract, tctx *contractapi.TransactionContext) *Funder { // TODO: Make polling interval adjustable
+func NewFunder(c contractapi.Contract) *Funder { // TODO: Make polling interval adjustable
 	return &Funder{
-		contract: assset.NewAssetHolder(c), // Network is already a gateway connection of a specific acc. Maybe gen gateway here... ?
-		tctx:     tctx,                     // Created when a smart contract is deployed to a channel and made available to every subsequent transaction invocation.
+		contract: asset.NewAssetHolder(c),
 		polling:  defaultPollingInterval,
 	}
 }
@@ -38,23 +36,28 @@ func (f *Funder) Fund(ctx context.Context, req pchannel.FundingReq) error {
 	assetIndex := 0
 	funding := req.Agreement[assetIndex][req.Idx]
 
+	// Get transaction context.
+	// Created when a smart contract is deployed to a channel and made available to every subsequent transaction invocation.
+	_tctx := f.contract.GetTransactionContextHandler()
+	tctx, _ := _tctx.(*contractapi.TransactionContext)
+
 	// Make deposit.
-	err := f.contract.Deposit(f.tctx, id, funding.String()) // TODO: Check why Deposit required the funding in string format
+	err := f.contract.Deposit(tctx, id, funding.String()) // TODO: Check why Deposit required the funding in string format
 	if err != nil {
 		return err
 	}
 
 	// Wait for Funding completion.
-	return f.awaitFundingComplete(ctx, req)
+	return f.awaitFundingComplete(ctx, tctx, req)
 }
 
 // awaitFundingComplete blocks until the funding of the specified channel is complete.
-func (f *Funder) awaitFundingComplete(ctx context.Context, req pchannel.FundingReq) error {
+func (f *Funder) awaitFundingComplete(ctx context.Context, tctx *contractapi.TransactionContext, req pchannel.FundingReq) error {
 	total := req.State.Allocation.Sum()[0] // [0] because we are only supporting one asset.
 	for {
 		funded := big.NewInt(0) // TODO: We need to use a asset type here.
 		for i := range req.Params.Parts {
-			fundedStr, err := f.contract.Holding(f.tctx, req.Params.ID(), req.Params.Parts[i].String()) // TODO: We could use "TotalHolding" here to replace the loop
+			fundedStr, err := f.contract.Holding(tctx, req.Params.ID(), req.Params.Parts[i].String()) // TODO: We could use "TotalHolding" here to replace the loop
 			if err != nil {
 				log.Printf("Warning: Error querying deposit: %v\n", err)
 			}
