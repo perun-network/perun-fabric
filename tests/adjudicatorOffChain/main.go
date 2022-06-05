58d71a57caa27c5ba4c5c4549e0fc80153c101ae
Leandro Rometsch <leandro@perun.network>
2022-06-05 23:13:16 +0200

    adj/test: first working version
    Signed-off-by: Leandro Rometsch <leandro@perun.network>

--- tests/adjudicatorOffChain/main.go
+++ tests/adjudicatorOffChain/main.go
@@ -38,7 +38,7 @@ func main() {
 		adjtest.WithBalances(big.NewInt(4000), big.NewInt(1000)))
 	ch, id := setup.SignedChannel(), setup.State.ID
 
-	log.Printf("Depositing channel: %+v", ch)
+	log.Printf("Depositing channel ...")
 	for i, part := range setup.Parts {
 		bal := setup.State.Balances[i]
 		tests.FatalClientErr("sending Deposit tx", adjs[i].Binding.Deposit(id, bal))
@@ -48,13 +48,18 @@ func main() {
 		log.Printf("Queried holding[%d]: %v", i, holding)
 		tests.RequireEqual(bal, holding, "Holding")
 	}
-
+	log.Printf("Depositing channel - Successful")
+	fmt.Println("")
 	subChannels := []pchannel.SignedState{} // We do not test with subchannels yet.
 
 	// Adjudicator: Subscribe to events.
+	log.Println("Subscription: Init ...")
 	eventSub, err := adjs[0].Adjudicator.Subscribe(ctx, id)
 	tests.FatalErr("subscribe", err)
+	log.Println("Subscription: Init - Successful")
+	fmt.Println("")
 
+	log.Println("Register: Version 0 ...")
 	// Adjudicator: Register version 0.
 	{
 		req := pchannel.AdjudicatorReq{
@@ -67,21 +72,23 @@ func main() {
 		err := adjs[0].Adjudicator.Register(ctx, req, subChannels)
 		tests.FatalErr("register version 0", err)
 	}
+	log.Println("Register: Version 0 - Successful")
+	fmt.Println("")
 
-	reg, _ := adjs[0].Binding.StateReg(id)
-	fmt.Println(reg)
-	fmt.Println("0")
 	// Subscription: Check registered event version 0.
 	{
+		log.Println("Subscription: Check RegisteredEvent ...")
 		e, ok := eventSub.Next().(*pchannel.RegisteredEvent)
 		tests.RequireEqual(true, ok, "registered")
 		tests.RequireEqual(e.ID() == ch.Params.ID(), true, "equal ID")
 		tests.RequireEqual(e.Version() == ch.State.Version, true, "version")
 		tests.RequireEqual(e.State.Equal(ch.State.CoreState()) == nil, true, "equal state")
+		log.Println("Subscription: Check RegisteredEvent - Successful")
+		fmt.Println("")
 	}
 
-	fmt.Println("1")
 	// Adjudicator: Register version 1.
+	log.Println("Register: Version 1 ...")
 	setup.State.Version = 1
 	setup.State.IsFinal = false
 	setup.State.Balances = []pchannel.Bal{big.NewInt(3000), big.NewInt(2000)}
@@ -97,7 +104,22 @@ func main() {
 		err = adjs[1].Adjudicator.Register(ctx, req, subChannels)
 		tests.FatalClientErr("register version 1", err)
 	}
+	log.Println("Register: Version 1 - Successful")
+	fmt.Println("")
 
+	// Subscription: Check registered event version 1 and wait for timeout.
+	{
+		log.Println("Subscription: Check RegisteredEvent ...")
+		e, ok := eventSub.Next().(*pchannel.RegisteredEvent)
+		tests.RequireEqual(true, ok, "registered")
+		tests.RequireEqual(e.ID() == ch.Params.ID(), true, "equal ID")
+		tests.RequireEqual(e.Version() == ch.State.CoreState().Version, true, "version")
+		tests.RequireEqual(e.State.Equal(ch.State.CoreState()) == nil, true, "equal state")
+		log.Println("Subscription: Check RegisteredEvent - Successful")
+		fmt.Println("")
+	}
+
+	log.Println("Check holdings ...")
 	for i, part := range setup.Parts {
 		bal := setup.State.Balances[i]
 		holding, err := adjs[i].Binding.Holding(id, part)
@@ -105,22 +127,13 @@ func main() {
 		log.Printf("Queried holding[%d]: %v", i, holding)
 		tests.RequireEqual(bal, holding, "Holding")
 	}
-
-	// Subscription: Check registered event version 1 and wait for timeout.
-	{
-		e, ok := eventSub.Next().(*pchannel.RegisteredEvent)
-		tests.RequireEqual(true, ok, "registered")
-		tests.RequireEqual(e.ID() == ch.Params.ID(), true, "equal ID")
-		tests.RequireEqual(e.Version() == ch.State.CoreState().Version, true, "version")
-		tests.RequireEqual(e.State.Equal(ch.State.CoreState()) == nil, true, "equal state")
-		//err = e.Timeout().Wait(ctx)
-		//tests.FatalErr("registered: wait", err)
-	}
-
+	log.Println("Check holdings - Successful")
+	fmt.Println("")
 	// Adjudicator: Progress.
 	// We do not test on-chain progression yet.
 
 	// Adjudicator: Withdraw.
+	// Withdraw can take some time (because it is waiting for conclusion)
 	{
 		req := pchannel.AdjudicatorReq{
 			Params: ch.Params.CoreParams(),
@@ -133,19 +146,25 @@ func main() {
 		for _, i := range rand.Perm(numParts) {
 			req.Idx = pchannel.Index(i)
 			req.Acc = adjs[i].Account
+			log.Printf("Withdraw: Client %d ... (takes some time because of dispute)", req.Idx)
 			err = adjs[i].Adjudicator.Withdraw(ctx, req, MakeStateMapFromSignedStates(subChannels...))
 			tests.FatalClientErr("withdraw", err)
 		}
+		log.Println("Withdraw - Successful")
+		fmt.Println("")
 	}
 
 	// Subscription: Check concluded event.
 	{
+		log.Println("Subscription: Check ConcludedEvent ...")
 		e, ok := eventSub.Next().(*pchannel.ConcludedEvent)
 		tests.RequireEqual(true, ok, "concluded")
 		tests.RequireEqual(e.ID() == ch.Params.ID(), true, "equal ID")
 		tests.RequireEqual(e.Version() == ch.State.CoreState().Version, true, "version")
 		err = e.Timeout().Wait(ctx)
 		tests.FatalErr("concluded: wait", err)
+		log.Println("Subscription: Check ConcludedEvent - Successful")
+		fmt.Println("")
 	}
 
 	// Subscription: Close.
